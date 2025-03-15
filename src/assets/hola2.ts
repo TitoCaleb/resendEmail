5802bd874c53ed5735fe0c239cb50f307e4dbbd9
TitoCaleb <calebcastro2604@gmail.com>
2025-03-15 16:28:50 -0500

    feat: add changes

--- src/assets/hola2.ts
+++ src/assets/hola2.ts
@@ -1,1 +1,11 @@
 console.log("hola Titin");
+
+const archivos = [
+  { nombre: "archivo1.txt", tamaño: 100 },
+  { nombre: "archivo2.txt", tamaño: 200 },
+  { nombre: "archivo3.txt", tamaño: 300 },
+];
+
+archivos.forEach((archivo) => {
+  console.log(archivo.nombre);
+});
